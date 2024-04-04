04fc01e6836cb37d4c86fa2d25542d3137e900b6
Anish Soni <anish.21cse189@rtu.ac.in>
2024-04-05 00:00:11 +0530

    navbar cant be used as jsx component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Navbar from "@/components/global/navbar";
 
 export default function Home() {
   return (
     <main>
-      <Navbar> </Navbar>
+      <Navbar />
     </main>
   );
 }
