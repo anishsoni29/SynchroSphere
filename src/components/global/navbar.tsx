e923ee881cdfd488e42134c65799e502c9abb55e
Anish Soni <anish.21cse189@rtu.ac.in>
2024-04-04 23:57:59 +0530

    added the navbar in the components

---
+++ src/components/global/navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Navbar = async (props: Props) => {
+  return (
+    <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100]">
+      {" "}
+    </header>
+  );
+};
+
+export default Navbar;
