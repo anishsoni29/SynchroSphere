46b81e4516f4e5577b0ad4066bbe56bda13407aa
Anish Soni <anish.21cse189@rtu.ac.in>
2024-04-10 23:30:07 +0530

    added image in the navbar container

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Image from "next/image";
+
 type Props = {};
 
 const Navbar = async (props: Props) => {
@@ -7,6 +9,13 @@ const Navbar = async (props: Props) => {
     <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
       <aside className="flex items-center gap-[2px]">
         <p className="text-3xl font-bold"> Fu</p>
+        <Image
+          src="/assets/images/logo.png"
+          alt="logo"
+          width={30}
+          height={30}
+          className="shadow-sm"
+        />
       </aside>
     </header>
   );
