87ccee3ef257d6f775c406e0afb4d1aaedceebb7
Anish Soni <anish.21cse189@rtu.ac.in>
2024-04-13 12:49:34 +0530

    added animated classes in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,15 @@ export default function Home() {
   return (
     <main>
       <Navbar />
-      <section className="h-screen w-full bg-neutral-950 rounded-md !overflow-visible relative flex flex-col items-center antialiased">
-        <div className="max-w-4xl mx-auto p-4 relative z-10 flex items-center justify-center flex-col"></div>
+      <section
+        className="h-screen w-full bg-neutral-950 rounded-md loverflow-visible relative flex flex-col items-center
+    antialiased"
+      >
+        <div
+          className="absolute inset-0 h-full w-full items-center px-5 py-24 [background: radial-gradient (125%_125%_at_50%_10%,
+    #000_35%, #223_100%)]"
+        ></div>
+        <div className="flex flex-col mt-[-100px] md:mt-[-50px]"></div>
       </section>
     </main>
   );
