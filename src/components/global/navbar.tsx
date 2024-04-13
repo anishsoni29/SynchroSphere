5256a1296d2df3b51f0c0c8177f3436fcba1d059
Anish Soni <anish.21cse189@rtu.ac.in>
2024-04-13 12:34:20 +0530

    changed the design margin of dashboard

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -44,10 +44,22 @@ const Navbar = async (props: Props) => {
       <aside className="flex items-center gap-4">
         <Link
           href="/dashboard"
-          className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus: ring-offset-2 focus:ring-offset-slate-50"
+          className="relative inline-flex h-10 overflow-hidden rounded-full p-[2x] focus:outline-none focus: ring-2
+          •focus: ring-slate-400 focus: ring-offset-2
+          •focus: ring-offset-slate-50"
         >
-          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%, #E2CBFF_0%, #393BB2_50%, #E2CBFF_100%)] "></span>
-          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white background-blur-3xl">
+          <span
+            className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient (from_90deg_at
+            _50%_50%, #E2CBFF.
+            _0%, #393BB2.
+            -_50%, #E2CBFF_100%)]"
+          ></span>
+
+          <span
+            className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full @bg-slate-950 px-3
+          py-1 text-sm font-medium
+          text-white backdrop-blur-3x1"
+          >
             {true ? "Dashboard" : "Get Started"}
           </span>
         </Link>
