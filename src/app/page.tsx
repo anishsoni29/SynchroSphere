4cd30fd4a8e6c0c7e4e9fcab241b33132b28a291
Anish Soni <anish.21cse189@rtu.ac.in>
2024-04-13 12:45:29 +0530

    added section with style below the navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ export default function Home() {
   return (
     <main>
       <Navbar />
+      <section className="h-screen w-full bg-neutral-950 rounded-md !overflow-visible relative flex flex-col items-center antialiased">
+        <div className="max-w-4xl mx-auto p-4 relative z-10 flex items-center justify-center flex-col"></div>
+      </section>
     </main>
   );
 }
