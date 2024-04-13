87f253e10ad4d00dccc62b627f3dc93936675f38
Anish Soni <anish.21cse189@rtu.ac.in>
2024-04-13 13:53:18 +0530

    added the homescreen elements with animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,11 +12,18 @@ export default function Home() {
         <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
           <ContainerScroll
             titleComponent={
-              <div className="flex items-center justify-center flex-col">
+              <div className="flex items-center flex-col">
                 <Button
                   size={"lg"}
-                  className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
-                ></Button>
+                  className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full @border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
+                >
+                  <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600 md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black group-hover:to-black">
+                    Start For Free Today
+                  </span>
+                </Button>
+                <h1 className="text-5xl md:text-8xl bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
+                  Automate Your Work With Synchro
+                </h1>
               </div>
             }
           ></ContainerScroll>
