fc689edd72ed64a678724659b3a3d49ba309b28c
Anish Soni <anish.21cse189@rtu.ac.in>
2024-04-13 13:29:21 +0530

    added container scroll and styles to the dashboard icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,8 @@ export default function Home() {
   return (
     <main>
       <Navbar />
-      <section
-        className="h-screen w-full bg-neutral-950 rounded-md loverflow-visible relative flex flex-col items-center
-    antialiased"
-      >
-        <div
-          className="absolute inset-0 h-full w-full items-center px-5 py-24 [background: radial-gradient (125%_125%_at_50%_10%,
-    #000_35%, #223_100%)]"
-        ></div>
+      <section className="h-screen w-full bg-neutral-950 rounded-md loverflow-visible relative flex flex-col items-center antialiased">
+        <div className="absolute inset-0 h-full w-full items-center px-5 py-24 [background: radial-gradient (125%_125%_at_50%_10%, #000_35%, #223_100%)]"></div>
         <div className="flex flex-col mt-[-100px] md:mt-[-50px]"></div>
       </section>
     </main>
