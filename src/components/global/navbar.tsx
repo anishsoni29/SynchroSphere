a4213667ebae96a24af3e3812b84b1f96f06ac9d
Anish Soni <anish.21cse189@rtu.ac.in>
2024-04-13 12:28:43 +0530

    imported currentUser from clerk and added design to the aside element at the end of the header

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import Image from "next/image";
 import Link from "next/link";
+import { currentUser } from "@clerk/nextjs";
 
 type Props = {};
 
 const Navbar = async (props: Props) => {
+  const user = await currentUser();
   return (
     <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
       <aside className="flex items-center gap-[2px]">
@@ -41,7 +43,17 @@ const Navbar = async (props: Props) => {
           </li>
         </ul>
       </nav>
-      <aside className="flex items-center gap-4"></aside>
+      <aside className="flex items-center gap-4">
+        <Link
+          href="/dashboard"
+          className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus: ring-offset-2 focus:ring-offset-slate-50"
+        >
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%, #E2CBFF_0%, #393BB2_50%, #E2CBFF_100%)] "></span>
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white background-blur-3xl">
+            {user ? "Dashboard" : "Get Started"}
+          </span>
+        </Link>
+      </aside>
     </header>
   );
 };
