b8c6393c56bbeb4649618db273a98c681d621812
Anish Soni <anish.21cse189@rtu.ac.in>
2024-04-23 23:18:38 +0530

    added the thumbnail constants

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,5 +1,51 @@
 //importing all the images in the public folder via this function
 
+import { link } from "fs";
+import { title } from "process";
+
 export const clients = [...new Array(10)].map((clients, index) => ({
   href: `/${index + 1}.png`,
 }));
+
+export const products = [
+  {
+    title: " Moonbeam",
+    link: "https://moonbeam.network/",
+    thumbnail: "/p1.png",
+  },
+  {
+    title: " Cursor",
+    link: "https://moonbeam.network/",
+    thumbnail: "/p1.png",
+  },
+  {
+    title: " Rogue",
+    link: "https://moonbeam.network/",
+    thumbnail: "/p1.png",
+  },
+  {
+    title: " Editorially",
+    link: "https://moonbeam.network/",
+    thumbnail: "/p1.png",
+  },
+  {
+    title: " Editrix AI",
+    link: "https://moonbeam.network/",
+    thumbnail: "/p1.png",
+  },
+  {
+    title: " Pixel Perfect",
+    link: "https://moonbeam.network/",
+    thumbnail: "/p1.png",
+  },
+  {
+    title: "Algochurn",
+    link: "https://moonbeam.network/",
+    thumbnail: "/p1.png",
+  },
+  {
+    title: "Aceternity UI",
+    link: "https://moonbeam.network/",
+    thumbnail: "/p1.png",
+  },
+];
