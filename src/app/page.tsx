7e1a6f9a8cebe7a4cceb48179a0a09ec95204d0e
Anish Soni <anish.21cse189@rtu.ac.in>
2024-04-25 14:42:36 +0530

    added the lamp component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,6 +41,9 @@ export default function Home() {
       <section>
         <HeroParallax products={products}></HeroParallax>
       </section>
+      <section className="mt-[-500px]">
+        <LampComponent />
+      </section>
     </main>
   );
 }
