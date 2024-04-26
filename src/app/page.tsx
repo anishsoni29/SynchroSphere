1a0f82a0fde0b933ea06692eef356872dc47d6c9
Anish Soni <anish.21cse189@rtu.ac.in>
2024-04-26 23:54:43 +0530

    added the top margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
       </section>
       <section className="mt-[-500px]">
         <LampComponent />
-        <div className="flex flex-wrap items-center justify-center flex-col md:flex-row gap-8"></div>
+        <div className="flex flex-wrap items-center justify-center flex-col md:flex-row gap-8 -mt-72"></div>
       </section>
     </main>
   );
