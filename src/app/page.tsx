c256d3016f1b13dfd2ef13847662c5043d4946b4
Anish Soni <anish.21cse189@rtu.ac.in>
2024-04-28 16:51:48 +0530

    added the hobby card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -76,6 +76,24 @@ export default function Home() {
                 </li>
               </ul>
             </CardItem>
+            <div className="flex justify-between items-center mt-8">
+              <CardItem
+                translateZ={20}
+                as="button"
+                className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+              >
+                {" "}
+                Try now ÷{" "}
+              </CardItem>
+              <CardItem
+                translateZ={20}
+                as="button"
+                className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+              >
+                {" "}
+                Get Started Now
+              </CardItem>
+            </div>
           </CardBody>
         </CardContainer>
       </section>
