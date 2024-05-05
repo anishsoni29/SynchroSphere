29a10f44d5e6004cf604fb99fd6029b2f83faee9
Anish Soni <anish.21cse189@rtu.ac.in>
2024-05-05 20:34:22 +0530

    added the GitBranch icon

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Link, LucideMousePointerClick } from "lucide-react";
+import { GitBranch, Link, LucideMousePointerClick } from "lucide-react";
 import { usePathname } from "next/navigation";
 import React from "react";
 import {
@@ -59,6 +59,11 @@ const MenuOptions = (props: Props) => {
         <div className="flex items-center flex-col gap-9 dark:bg-[#353346]/30 py-4 rounded-full h-56 overflow-scroll border-[1px]">
           <div className="relative dark: bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
             <LucideMousePointerClick className="dark : text-white" size={18} />
+            <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[-30px]"></div>
+          </div>
+          <div className="relative dark: bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <GitBranch className="dark : text-white" size={18} />
+            <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[-30px]"></div>
           </div>
         </div>
       </div>
